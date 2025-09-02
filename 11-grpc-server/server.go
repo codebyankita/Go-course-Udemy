b9ebdf8b4de1b941de6cb047a8b4bc965525c6ca
CodeByAnkita <ankitacode11@gmail.com>
2025-09-02 15:38:09 +0700

    added more thing from request and response side but only for sample structure

--- 11-grpc-server/server.go
+++ 11-grpc-server/server.go
@@ -1,22 +1,40 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 
+	pb "simplegrpcserver/proto/gen"
+
 	"google.golang.org/grpc"
 )
-func main(){
-	port:= ":50051"
-	lis, err := net.Listen("tcp",port)
+
+type server struct {
+	pb.UnimplementedCalculateServer
+}
+
+func (s *server) Add(ctx context.Context, req *pb.AddRequest) (*pb.AddResponse, error) {
+	sum := req.A + req.B
+	log.Println("Sum:", sum)
+	return &pb.AddResponse{
+		Sum: sum,
+	}, nil
+}
+func main() {
+	port := ":50051"
+	lis, err := net.Listen("tcp", port)
 
 	if err != nil {
-		log.Fatal("Failed to listen:", err)}
-grpcserver := grpc.NewServer()
+		log.Fatal("Failed to listen:", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterCalculateServer(grpcServer, &server{})
 
-log.Println("Server is running on port", port)
-err = grpcserver.Serve(lis)
-if err != nil {
-		log.Fatal("Failed to listen:", err)}
-		
+	log.Println("Server is running on port", port)
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		log.Fatal("Failed to listen:", err)
+	}
+
 }
