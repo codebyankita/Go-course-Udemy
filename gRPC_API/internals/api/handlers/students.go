20c134a3dd733e76531c63c340cb1f8a4fbaf344
CodeByAnkita <ankitacode11@gmail.com>
2025-09-15 13:01:36 +0700

    Added EXECS

--- gRPC_API/internals/api/handlers/students.go
+++ gRPC_API/internals/api/handlers/students.go
@@ -2,7 +2,7 @@ package handlers
 
 import (
 	"context"
-	"grpcapi/internals/models"
+	// "grpcapi/internals/models"
 	"grpcapi/internals/repositories/mongodb"
 	pb "grpcapi/proto/gen"
 
@@ -25,58 +25,58 @@ func (s *Server) AddStudents(ctx context.Context, req *pb.Students) (*pb.Student
 	return &pb.Students{Students: addedStudents}, nil
 }
 
-func (s *Server) GetStudents(ctx context.Context, req *pb.GetStudentsRequest) (*pb.Students, error) {
-	// Filtering, getting the filters from the request, another function
-	filter, err := buildFilter(req.Student, &models.Student{})
-	if err != nil {
-		return nil, status.Error(codes.InvalidArgument, err.Error())
-	}
-	// Sorting, getting the sort options from the request, another function
-	sortOptions := buildSortOptions(req.GetSortBy())
-	// Access the database to fetch data, another function
+// func (s *Server) GetStudents(ctx context.Context, req *pb.GetStudentsRequest) (*pb.Students, error) {
+// 	// Filtering, getting the filters from the request, another function
+// 	filter, err := buildFilter(req.Student, &models.Student{})
+// 	if err != nil {
+// 		return nil, status.Error(codes.InvalidArgument, err.Error())
+// 	}
+// 	// Sorting, getting the sort options from the request, another function
+// 	sortOptions := buildSortOptions(req.GetSortBy())
+// 	// Access the database to fetch data, another function
 
-	pageNumber := req.GetPageNumber()
-	pageSize := req.GetPageSize()
+// 	pageNumber := req.GetPageNumber()
+// 	pageSize := req.GetPageSize()
 
-	if pageNumber < 1 {
-		pageNumber = 1
-	}
-	if pageSize < 1 {
-		pageSize = 10
-	}
+// 	if pageNumber < 1 {
+// 		pageNumber = 1
+// 	}
+// 	if pageSize < 1 {
+// 		pageSize = 10
+// 	}
 
-	students, err := mongodb.GetStudentsFromDb(ctx, sortOptions, filter, pageNumber, pageSize)
-	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
-	}
+// 	students, err := mongodb.GetStudentsFromDb(ctx, sortOptions, filter, pageNumber, pageSize)
+// 	if err != nil {
+// 		return nil, status.Error(codes.Internal, err.Error())
+// 	}
 
-	return &pb.Students{Students: students}, nil
-}
+// 	return &pb.Students{Students: students}, nil
+// }
 
-func (s *Server) UpdateStudents(ctx context.Context, req *pb.Students) (*pb.Students, error) {
+// func (s *Server) UpdateStudents(ctx context.Context, req *pb.Students) (*pb.Students, error) {
 
-	updatedStudents, err := mongodb.ModifyStudentsInDb(ctx, req.Students)
-	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
-	}
+// 	updatedStudents, err := mongodb.ModifyStudentsInDb(ctx, req.Students)
+// 	if err != nil {
+// 		return nil, status.Error(codes.Internal, err.Error())
+// 	}
 
-	return &pb.Students{Students: updatedStudents}, nil
-}
+// 	return &pb.Students{Students: updatedStudents}, nil
+// }
 
-func (s *Server) DeleteStudents(ctx context.Context, req *pb.StudentIds) (*pb.DeleteStudentsConfirmation, error) {
-	// ids := req.GetIds()
-	// var studentIdsToDelete []string
-	// for _, student := range ids {
-	// 	studentIdsToDelete = append(studentIdsToDelete, student.Id)
-	// }
+// func (s *Server) DeleteStudents(ctx context.Context, req *pb.StudentIds) (*pb.DeleteStudentsConfirmation, error) {
+// 	// ids := req.GetIds()
+// 	// var studentIdsToDelete []string
+// 	// for _, student := range ids {
+// 	// 	studentIdsToDelete = append(studentIdsToDelete, student.Id)
+// 	// }
 
-	deletedIds, err := mongodb.DeleteStudentsFromDb(ctx, req.GetIds())
-	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
-	}
+// 	deletedIds, err := mongodb.DeleteStudentsFromDb(ctx, req.GetIds())
+// 	if err != nil {
+// 		return nil, status.Error(codes.Internal, err.Error())
+// 	}
 
-	return &pb.DeleteStudentsConfirmation{
-		Status:     "Students successfully deleted",
-		DeletedIds: deletedIds,
-	}, nil
-}
+// 	return &pb.DeleteStudentsConfirmation{
+// 		Status:     "Students successfully deleted",
+// 		DeletedIds: deletedIds,
+// 	}, nil
+// }
